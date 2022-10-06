d26a21de088d6659f2a1aef5a0d3e34334b564d2
sw1860 <sw1860@rit.edu>
2022-10-06 01:21:09 -0400

    Implemented UpperCharNode and LowerCharNode.

--- src/Nodes/LowerCharNode.java
+++ src/Nodes/LowerCharNode.java
@@ -1,16 +1,39 @@
 package Nodes;
 
 import main.JottTree;
+import main.Token;
+import main.TokenType;
+
+import java.util.ArrayList;
 
 public class LowerCharNode implements JottTree {
 
-    public LowerCharNode() {
+    private final String character;
 
+    public LowerCharNode(String c) {
+        this.character = c;
+    }
+
+    public static LowerCharNode parseLowerCharNode (ArrayList<Token> tokens) throws Exception {
+        if (tokens.get(0).getTokenType() != TokenType.ID_KEYWORD){
+            return null;
+        }
+        else {
+            String value = tokens.get(0).getToken();
+            if(value.length() == 1) {
+                char c = value.charAt(0);
+                if (Character.isLowerCase(c)) {
+                    tokens.remove(0);
+                    return new LowerCharNode(value);
+                }
+            }
+        }
+        return null;
     }
 
     @Override
     public String convertToJott() {
-        return null;
+        return character;
     }
 
     @Override
