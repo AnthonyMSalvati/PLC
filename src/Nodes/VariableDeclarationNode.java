6beb544351e80542b522dc890074a0d468764848
Ben Froment <bjf1986@rit.edu>
2022-10-09 16:20:28 -0400

    Implemented VariableDeclarationNode

--- src/Nodes/VariableDeclarationNode.java
+++ src/Nodes/VariableDeclarationNode.java
@@ -1,15 +1,46 @@
 package Nodes;
 
 import main.JottTree;
+import main.Token;
+
+import java.util.ArrayList;
 
 public class VariableDeclarationNode implements JottTree {
 
-    public VariableDeclarationNode() {
+    private final TypeNode typeNode;
+    private final IdNode idNode;
+    private final EndStatementNode endStatementNode;
 
+    public VariableDeclarationNode(TypeNode typeNode, IdNode idNode, EndStatementNode endStatementNode) {
+        this.typeNode = typeNode;
+        this.idNode = idNode;
+        this.endStatementNode = endStatementNode;
+    }
+
+    public static VariableDeclarationNode parseVariableDeclarationNode(ArrayList<Token> tokens) {
+        TypeNode typeNode = TypeNode.parseTypeNode(tokens);
+        if (typeNode != null) {
+            IdNode idNode = IdNode.parseIdNode(tokens);
+            if (idNode != null) {
+                EndStatementNode endStatementNode = EndStatementNode.parseEndStatementNode(tokens);
+                if (endStatementNode != null) {
+                    return new VariableDeclarationNode(typeNode, idNode, endStatementNode);
+                }
+            }
+        }
+        return null;
     }
 
     @Override
     public String convertToJott() {
+        if (typeNode != null) {
+            if (idNode != null) {
+                if (endStatementNode != null) {
+                    return typeNode.convertToJott() + idNode.convertToJott() +
+                            endStatementNode.convertToJott();
+                }
+            }
+        }
         return null;
     }
 
