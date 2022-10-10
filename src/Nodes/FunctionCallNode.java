d6756a72cc3005b10979cd8a877e8c04a5572d5e
Ian Paap-Gray <iep8733@rit.edu>
2022-10-10 19:40:29 -0400

    Implemented FunctionCallNode

--- src/Nodes/FunctionCallNode.java
+++ src/Nodes/FunctionCallNode.java
@@ -7,17 +7,39 @@ import java.util.ArrayList;
 
 public class FunctionCallNode implements JottTree {
 
-    public FunctionCallNode() {
+	private final IdNode id;
+	private final ParameterNode params;
+	
+	public FunctionCallNode(IdNode id) {
+		this.id = id;
+		this.params = null;
+	}
 
+    public FunctionCallNode(IdNode id, ParameterNode params) {
+		this.id = id;
+		this.params = params;
     }
 
     public static FunctionCallNode parseFunctionCallNode(ArrayList<Token> tokens) throws Exception {
-
+		IdNode id = IdNode.parseIdNode(tokens);
+		if (id != null) {
+			params = ParameterNode.parseParameterNode(tokens);
+			if (params != null) {
+				return new FunctionCallNode(id, params);
+			}
+			return new FunctionCallNode(id);
+		}
         return null;
     }
 
     @Override
     public String convertToJott() {
+		if (this.id != null) {
+			if (this.params != null) {
+				return this.id.convertToJott() + this.params.convertToJott();
+			}
+			return this.id.convertToJott();
+		}
         return null;
     }
 
